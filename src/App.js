62409f551781c361d35faaa462f4f4b19839e77c
Amberf0114 <amber.freeman@bryanuniversity.edu>
2021-02-20 19:39:28 -0500

    First-Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Today's To-Do</h2>
+      <input type = "checkbox"/>Bath Day<br/>
+      <input type = "checkbox"/>Playtime<br/>
+      <input type = "checkbox"/>Rocket Science<br/>
     </div>
   );
 }
