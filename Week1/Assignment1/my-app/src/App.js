fb6e7deb37e913a32083c96fe6f697aab35d8770
Amberf0114 <amber.freeman@bryanuniversity.edu>
2021-02-25 20:47:20 -0500

    assign1

---
+++ Week1/Assignment1/my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h2>Today's To-Do</h2>
+      <input type = "checkbox"/>Bath Day<br/>
+      <input type = "checkbox"/>Playtime<br/>
+      <input type = "checkbox"/>Rocket Science<br/>
+    </div>
+  );
+}
+
+export default App;
