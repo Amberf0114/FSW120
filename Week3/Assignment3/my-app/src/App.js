58f678660451ac4c98be1c68eac4d6f105cff434
unknown <amber.freeman@bryanuniversity.edu>
2021-05-06 22:12:52 -0400

    mycommit

--- Week3/Assignment3/my-app/src/App.js
+++ Week3/Assignment3/my-app/src/App.js
@@ -7,12 +7,72 @@ class App extends React.Component {
     this.state = {
       colors: ['white','white','white','white']
     }
-    this.handleClick = this.handleClick.bind(this)
   }
 
-  handleClick = (event)=> {
-    let colors = [...this.state.colors]
+  handleClickSmall = ()=> {
+    let setColors 
+    this.setState(prevState => {
+      if(prevState.colors[0]!=="white"){
+        setColors = ['white','white','white','white' ]
+      }
+      else if (prevState.colors[0]=='white'){
+        setColors = ['black','black','black','black' ]
+      }
+      return {
+        colors: setColors
+      }
+    })
   }
+
+handleClickParty = ()=> {
+  let setColors
+  this.setState((prevState) => {
+    setColors = ['purple', 'purple', prevState.colors[2], prevState.colors[3]]
+    return {
+      colors: setColors
+    }
+  }) 
+}
+
+handleClickProLeft = () => {
+//this one has colors as an object so setColors is not necessary
+  this.setState((prevState) => ({
+    colors : [prevState.colors[0], prevState.colors[1], 'blue', prevState.colors[3]]
+  }))
+}
+
+handleClickProRight = () => {
+  //this one has colors as an object so setColors is not necessary
+  //return also is not necessary
+    this.setState((prevState) => ({
+      colors : [prevState.colors[0], prevState.colors[1], prevState.colors[2], 'blue']
+    }))
+  }
+
+  handleClick0 = () => {
+    this.setState((prevState) => ({
+      colors : ['red', 'white', 'white', 'red']
+    }))
+  }
+
+  handleClick1 = () => {
+    this.setState((prevState) => ({
+      colors : ['white', 'red', 'red', 'white']
+    }))
+  }
+
+  handleClick2 = () => {
+    this.setState((prevState) => ({
+      colors : ['black', 'white', 'white', 'black']
+    }))
+  }
+  handleClick3 = () => {
+    this.setState((prevState) => ({
+      colors : ['red', 'black', 'black', 'red']
+
+    }))
+  }
+
   render(){
 
     return (
@@ -28,17 +88,17 @@ class App extends React.Component {
         <div  className='container1'>
 
           <div className='left-panel'>
-            <div className='btnBoard'><button className='button' name='smallTime' onClick={this.handleClick}>Small Time DJ</button></div>
-            <div className='btnBoard'><button className='button' name='party' onClick={this.handleClick}>Party DJ</button></div>
-            <div className='btnBoard'><button className='button' name='professional0' onClick={this.handleClick}>Professional DJ I</button></div>
-            <div className='btnBoard'><button className='button' name='professional1' onClick={this.handleClick}>Professional DJ II</button></div>
+            <div className='btnBoard'><button className='button' name='smallTime' onClick={this.handleClickSmall}>Small Time DJ</button></div>
+            <div className='btnBoard'><button className='button' name='party' onClick={this.handleClickParty}>Party DJ</button></div>
+            <div className='btnBoard'><button className='button' name='professional0' onClick={this.handleClickProLeft}>Professional DJ I</button></div>
+            <div className='btnBoard'><button className='button' name='professional1' onClick={this.handleClickProRight}>Professional DJ II</button></div>
           </div>
 
           <div className='right-panel'>
-            <div className='btnBoard'><button className='button' name='bigTime0' onClick={this.handleClick}>Big Time DJ I</button></div>
-            <div className='btnBoard'><button className='button' name='bigTime1' onClick={this.handleClick}>Big Time DJ II</button></div>
-            <div className='btnBoard'><button className='button' name='bigTime2' onClick={this.handleClick}>Big Time DJ III</button></div>
-            <div className='btnBoard'><button className='button' name='bigTime3' onClick={this.handleClick}>Big Time DJ IV</button></div>
+            <div className='btnBoard'><button className='button' name='bigTime0' onClick={this.handleClick0}>Big Time DJ I</button></div>
+            <div className='btnBoard'><button className='button' name='bigTime1' onClick={this.handleClick1}>Big Time DJ II</button></div>
+            <div className='btnBoard'><button className='button' name='bigTime2' onClick={this.handleClick2}>Big Time DJ III</button></div>
+            <div className='btnBoard'><button className='button' name='bigTime3' onClick={this.handleClick3}>Big Time DJ IV</button></div>
           </div>
         </div>
   
